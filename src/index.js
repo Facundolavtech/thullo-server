a381109ed311e428133ba2c6dc5948916624b317
FacuNotebook <facuhlavagninodq@gmail.com>
2020-12-12 19:57:19 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 4000;
 require("./config/db");
+const cors = require("cors");
+
+//Enable CORS
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/users", require("./routes/users"));
